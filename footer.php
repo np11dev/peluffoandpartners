7fbbd9f17ba44568eb0e2c75394ea596462fb160
nientepopcorn <pompilio.andrea@gmail.com>
2021-01-27 15:37:44 +0100

    Inserimento loghi certificazione

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
-    Peluffo & Partners Architettura Srl / PIVA 09891730963
+    <img class="certificazione-mini" src="http://www.peluffoandpartners.com/wp-content/uploads/2021/01/loghicert_mini.png" /> Peluffo & Partners Architettura Srl / PIVA 09891730963
 </div>
 <?php wp_footer(); ?>
 	<?php include_once("analyticstracking.php") ?>
--- templ-intro.php
+++ templ-intro.php
@@ -19,7 +19,7 @@ get_header('intro'); ?>
 			<a href="<?php echo $homeurl;?>" class="pollice"><img src="<?php echo $templ_dir ?>/images/impronta_intro.png" /></a>
 		</div>
         <div id="footer">
-            Peluffo & Partners Architettura Srl / PIVA 09891730963
+        <img class="certificazione-mini" src="http://www.peluffoandpartners.com/wp-content/uploads/2021/01/loghicert_mini.png" /> Peluffo & Partners Architettura Srl / PIVA 09891730963
         </div>
 	</div>
     <?php //edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
